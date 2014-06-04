b62429e60a9c1cf41616fee808f286b9b13ccff8
gcolind <gcolin@pdx.edu>
2014-06-04 15:18:38 -0700

    Functionality Complete

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 var addTask = function () {
 
     var newCheckbox = document.createElement("input"); // create input element
-    newCheckbox.type = "checkbox"; // typeof checkbox
-    newCheckbox.value = document.getElementById("userDone").value; // 
-    document.getElementById("toDoList").appendChild(newCheckbox); // append checkbox to toBeDone
+    newCheckbox.type = "checkbox"; // type of checkbox
+    newCheckbox.value = document.getElementById("userDone").value; // Value of the checkbox is 'userDone'
+    document.getElementById("toDoList").appendChild(newCheckbox); // append checkbox to toDoList div
 
     var label = document.createElement('label');  // create label
-    label.htmlFor = document.getElementById("userDone").value; //
-    label.appendChild(document.createTextNode(document.getElementById("userDone").value));
+    label.htmlFor = document.getElementById("userDone").value; // label or userDone checkbox
+    label.appendChild(document.createTextNode(document.getElementById("userDone").value)); // append label to userDone
 
-    document.getElementById("toDoList").appendChild(label);  // 
-    document.getElementById("toDoList").appendChild(document.createElement("br")); 
+    document.getElementById("toDoList").appendChild(document.createTextNode (" "));
+    document.getElementById("toDoList").appendChild(label);  //print the label/checkbox
+    document.getElementById("toDoList").appendChild(document.createElement("br")); //new line (since its in the same id, if we ommit this it appends on same line)
+
+    document.getElementById("userDone").value = ""; // Clear textarea
 }
